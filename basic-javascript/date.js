eb931940402d1c20c40948918942746fc04a7c3e
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-15 14:47:08 +0530

    built-in-functions of math and date object

---
+++ basic-javascript/date.js
@@ -0,0 +1,30 @@
+console.log(new Date())
+console.log(new Date(2022,10,22,5,18,34))
+
+console.log(new Date(2022, 10, 22))
+console.log(new Date("November 13 2022"))
+console.log(new Date("2022-11-22"))
+console.log(new Date("march/25/2018"))
+console.log(new Date("25/march/2015"))
+
+const months = ["January", "February", "March", "April", "May", "June", "July",
+ "August", "September", "October", "November", "December"];
+
+ const weeks = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
+
+console.log(new Date().getFullYear())
+console.log(months[new Date().getMonth()])
+console.log(new Date().getDate())
+console.log(weeks[new Date().getDay()])
+console.log(new Date().getHours())
+console.log(new Date().getMinutes())
+console.log(new Date().getSeconds())
+console.log(new Date().getMilliseconds())
+console.log(new Date().getTime())
+
+console.log(new Date().toString())
+console.log(new Date().toDateString())
+console.log(new Date().toTimeString())
+console.log(new Date().toUTCString())
+console.log(new Date().toISOString())
+console.log(new Date().toJSON())
