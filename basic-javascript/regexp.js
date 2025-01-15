1dd27ebf5ee4468866fcaa6130bf84708c261c3c
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-15 14:54:55 +0530

    ES6 features and Regular Expressions

---
+++ basic-javascript/regexp.js
@@ -0,0 +1,13 @@
+//RegExp Modifiers and Properties
+var pattern = /ain/gim;
+var text = "the rain is in spain mainly in \nthe plain";
+let position = "";
+while (pattern.test(text) == true) {
+  position = position + "\nstarts the next search at: " + pattern.lastIndex;
+}
+
+console.log("pattern.global : ", pattern.global);
+console.log("pattern.multiline : ", pattern.multiline);
+console.log("pattern.ignoreCase : ", pattern.ignoreCase);
+console.log("pattern.source : ", pattern.source);
+console.log(position);
