b1c9d3464874a2b35163173f6b1fa2bcac3e6c26
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-17 01:04:21 +0530

    Error handling approach along with async-await keyword in two ways.

--- core-fundamental/async-await.js
+++ core-fundamental/async-await.js
@@ -1,8 +1,20 @@
-
-async function fetchapi(){
-  const data = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const response = await data.json()
-  console.log(response)
+async function fetchapi() {
+  try {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    const response = await data.json();
+    console.log(response);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
+fetchapi();
 
-fetchapi()
+
+// Some developer handles the error like this: 
+// Eventually async function will return a promise, so we can attach a catch() method to it.
+async function fetchapi() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const response = await data.json();
+  console.log(response);
+}
+fetchapi().catch((err) => console.log(err.message));
